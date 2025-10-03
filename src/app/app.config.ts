205926ee5d57377dcfcaf33f908586e771c1fc17
harpreetsingho7 <82938144+harpreetsingho7@users.noreply.github.com>
2025-10-03 10:00:39 +0530

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {firebaseConfig } from '../app/environment'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,16 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
      // 3. Initialize Firebase and Firestore
-    provideFirebaseApp(() => initializeApp({
-      apiKey: 'AIzaSyAGO9DrZie1NTHAznizXoVqm9PsnOY-i90',
-      authDomain: 'clicksrace.firebaseapp.com',
-      databaseURL: 'https://clicksrace-default-rtdb.firebaseio.com',
-      projectId: 'clicksrace',
-      storageBucket: 'clicksrace.firebasestorage.app',
-      messagingSenderId: '710637413847',
-      appId: '1:710637413847:web:986ed4197b1ccbd42f5792',
-      measurementId: 'G-5DLP28WPCF',
-    })),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideDatabase(() => getDatabase()),
     provideAnimations(), // required animations providers
     provideToastr(), // Toastr providers
---
+++ src/app/environment.ts
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+      apiKey: 'AIzaSyAGO9DrZie1NTHAznizXoVqm9PsnOY-i90',
+      authDomain: 'clicksrace.firebaseapp.com',
+      databaseURL: 'https://clicksrace-default-rtdb.firebaseio.com',
+      projectId: 'clicksrace',
+      storageBucket: 'clicksrace.firebasestorage.app',
+      messagingSenderId: '710637413847',
+      appId: '1:710637413847:web:986ed4197b1ccbd42f5792',
+      measurementId: 'G-5DLP28WPCF',
+    }
